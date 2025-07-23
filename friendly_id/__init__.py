b28e87e8b74e3a9bb7d8110b741e94af300886b4
Junlin Zhou <jameszhou2108@hotmail.com>
2025-07-23 13:45:59 +0800

    build: migrate build system to uv

--- friendly_id/__init__.py
+++ friendly_id/__init__.py
@@ -2,3 +2,9 @@ from friendly_id.friendly_id import friendly_id
 from friendly_id.friendly_id import encode, decode
 
 __version__ = "0.3.3"
+
+__all__ = [
+    "friendly_id",
+    "encode",
+    "decode",
+]
